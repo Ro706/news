4a5d6609d612f2b9a39d078f9f380f2b69251df7
Ro706 <rohitmandal99091@gmail.com>
2025-07-19 23:21:17 +0530

    v1

--- src/components/home/Hellotag.jsx
+++ src/components/home/Hellotag.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
 function Hellotag() {
   return (
     <div className='hellotag'>
-      <h1><a href="mailto:Team@newsmakermediagroup.com">Team@newsmakermediagroup.com</a></h1>
-      <h1><a href="tel:+919819416689" style={{ display: 'flex', alignItems: 'center' }}><img src="/assets/phone_logo.png" style={{ marginRight: '8px' }} crossOrigin="anonymous" width={60}/>+91 9819416689</a></h1>
-      <script src="https://kit.fontawesome.com/4f928f5798.js" crossOrigin="anonymous"></script>
+      <h1>
+        <a href="mailto:Team@newsmakermediagroup.com">
+          Team@newsmakermediagroup.com
+        </a>
+      </h1>
+      <h1>
+        <a href="tel:+919819416689" style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src="/assets/phone_logo.png"
+            style={{ marginRight: '8px' }}
+            crossOrigin="anonymous"
+            width={60}
+            alt="Phone"
+          />
+          +91 9819416689
+        </a>
+      </h1>
     </div>
   )
 }
